348313570fd5c722005c2ba022dc446d02f6214b
Mia Oh <a.mia.oh@gmail.com>
2024-04-16 18:24:56 +1000

    md styling update

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -5,11 +5,11 @@ export function NavigationBar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className=" w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-row p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-row p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-grey-100 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-white bg-scrabble-green rounded-lg :bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 px-3 text-white bg-scrabble-green rounded-lg :bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-blue-500"
               >
                 Home
               </Link>
