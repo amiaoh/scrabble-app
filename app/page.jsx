b175906648142d9ef1207f107fb9ed839e89db4e
Mia Oh <a.mia.oh@gmail.com>
2024-04-16 16:00:17 +1000

    error message for missing input

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,72 @@
 "use client";
-import { AllLetterInputs } from "@/components/AllLetterInputs";
 import { useState } from "react";
 import Link from "next/link";
 import { SearchHistory } from "@/components/SearchHistory";
 import { addQuery } from "./addQuery";
-
-import "tailwindcss/tailwind.css";
+import { LetterInput } from "@/components/LetterInput";
 
 export default function Home() {
-  const [letters, setLetters] = useState(["", ""]);
-  const joinedLetters = letters.join("");
+  const [playerLetters, setPlayerLetters] = useState("");
+  const [boardLetter, setBoardLetter] = useState("");
+  const joinedLetters = playerLetters.concat(boardLetter);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const anyInputIsEmpty =
+    playerLetters.length === 0 || boardLetter.length === 0;
   return (
     <div>
       <h1>Scrabble Solver</h1>
       <p>This is the home page</p>
-      <AllLetterInputs
-        value={letters}
-        onChange={(newInput) => setLetters(newInput)}
-      />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          className="m-2"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <label htmlFor="player-letters">Player Letters</label>
+          <LetterInput
+            placeholder="scrabbl"
+            width="6rem"
+            name="player-letters"
+            value={playerLetters}
+            onChange={(newPlayerLetters) => {
+              if (newPlayerLetters.length <= 7) {
+                setPlayerLetters(newPlayerLetters);
+              }
+            }}
+          />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <label htmlFor="player-letters">Board Letter</label>
+          <LetterInput
+            placeholder="e"
+            width="1rem"
+            name="board-letters"
+            value={boardLetter}
+            onChange={(newBoardLetter) => {
+              if (newBoardLetter.length <= 1) {
+                setBoardLetter(newBoardLetter);
+              }
+            }}
+          />
+        </div>
+      </div>
       <Link
         onClick={async () => {
+          if (anyInputIsEmpty) {
+            setErrorMessage(
+              "Please enter 1-7 player letters and 1 board letter"
+            );
+            return;
+          }
           await addQuery(joinedLetters);
         }}
-        href={`/${joinedLetters}`}
+        href={anyInputIsEmpty ? `/` : `/${joinedLetters}`}
       >
         Submit
       </Link>
+
+      <div style={{ color: "red" }}>
+        {errorMessage ? <p>{errorMessage}</p> : null}
+      </div>
       <SearchHistory />
     </div>
   );
